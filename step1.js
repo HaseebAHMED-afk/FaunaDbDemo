a21894221577c840276a4234c670abed44ff384d
HaseebAHMED-afk <haseeb.ahmed456123@gmail.com>
2021-02-17 23:13:59 +0500

    Step1 completed

--- step1.js
+++ step1.js
@@ -6,6 +6,25 @@ q = faunadb.query;
 
 (
     async () => {
-        if(process.env.MY_ADMIN_SECRET)
+        if(process.env.MY_ADMIN_SECRET){
+            var client = new faunadb.Client({secret: process.env.MY_ADMIN_SECRET})
+
+
+            try {
+                var result = await client.query(
+                    q.CreateDatabase({name: 'MyTestDatabase'})
+                )
+                console.log(result);
+            } catch (error) {
+                if(error.requestResult.statusCode === 400 && error.message === 'instance already exist'){
+                    console.log('Database with this name already exists');
+                }else{
+                    console.log('Unknown Error');
+                    console.log(error);
+                }
+            }
+        }else{
+            console.log('No FaunaDB secret in .env file. Skipping DB setup');
+        }
     }
-)
+)();
